e38edbe96ba975b2fc5d6636d2bdba59642b93b8
Iliya-Yadegari <naruto3@email.com>
2021-07-12 19:06:52 +0100

    Create drawLeftSlantTriangle.py

---
+++ drawLeftSlantTriangle.py
@@ -0,0 +1,11 @@
+def drawLeftSlantTriangle(h):
+    
+    triangle = ''
+    asterik = '*'
+    
+    for i in range(h):
+        triangle += asterik
+        
+        print(triangle)
+
+drawLeftSlantTriangle(4)
